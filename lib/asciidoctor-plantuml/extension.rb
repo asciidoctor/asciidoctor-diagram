4b0596d7e6d92c9258b88d16b053c02359e5be84
Pepijn Van Eeckhoudt <pepijn@vaneeckhoudt.net>
2014-02-07 22:06:34 +0100

    Add support for ASCII art output formats (not very useful, but cute nevertheless)

--- lib/asciidoctor-plantuml/extension.rb
+++ lib/asciidoctor-plantuml/extension.rb
@@ -5,6 +5,13 @@ module Asciidoctor
   module PlantUml
     PLANTUML_JAR_PATH = File.expand_path File.join('..', 'plantuml.jar'), File.dirname(__FILE__)
 
+    PLANTUML_PARAMS = {
+        :txt => [:literal, '-tutxt', Encoding::UTF_8],
+        :utxt => [:literal, '-tutxt', Encoding::UTF_8],
+        :svg => [:image, '-tsvg', Encoding::ASCII_8BIT],
+        :png => [:image, nil, Encoding::ASCII_8BIT]
+    }
+
     class Block < Asciidoctor::Extensions::BlockProcessor
       option :contexts, [:listing, :literal, :open]
       option :content_model, :simple
@@ -12,41 +19,47 @@ module Asciidoctor
       option :default_attrs, {'format' => 'png'}
 
       def process(parent, reader, attributes)
-        plantuml_lines = reader.lines
-        format = attributes['format']
-        image_name = "#{attributes['target'] || file_name(plantuml_lines)}.#{format}"
-        result = plantuml(plantuml_lines * "\n", image_name, document.attributes['imagesdir'] || '', format)
+        plantuml_lines = reader.lines * "\n"
+        format = attributes.delete('format')
+        target = attributes.delete('target')
 
-        Asciidoctor::Block.new(parent, result[:type], :source => result[:source], :attributes => attributes)
+        block_type, format_flag, encoding = PLANTUML_PARAMS[format.to_sym]
+
+        result = plantuml(plantuml_lines, format_flag)
+        result.force_encoding(encoding)
+
+        case block_type
+          when :image
+            image_name = "#{target || file_name(plantuml_lines)}.#{format}"
+            image_dir = document.attributes['imagesdir'] || ''
+            File.open(File.expand_path(image_name, image_dir), "w") { |f| f.write result }
+
+            attributes['target'] = image_name
+            Asciidoctor::Block.new parent, :image, :content_model => :empty, :attributes => attributes
+          when :literal
+            Asciidoctor::Block.new parent, :literal, :source => result, :attributes => attributes
+          else
+            raise "Unsupported block type: #{block_type}"
+        end
       end
 
       private
 
-      def plantuml(code, file_name, image_dir, format)
+      def plantuml(code, format_flag)
         java_home = ENV['JAVA_HOME'] or raise 'The JAVA_HOME environment variable should be set to a JRE or JDK installation path.'
         java_cmd = File.expand_path('bin/java', java_home)
 
-        format_flag = case format
-                        when 'svg'
-                          ' -tsvg'
-                        else
-                          ''
-                      end
+        cmd = "#{java_cmd} -jar " + PLANTUML_JAR_PATH + " -charset UTF-8 -failonerror -pipe"
+        cmd << " #{format_flag}" if format_flag
 
-        cmd = "#{java_cmd} -jar " + PLANTUML_JAR_PATH + " -failonerror -pipe" + format_flag
-
-        result, status = Open3.capture2e(cmd, :stdin_data=>code)
-        if status.exitstatus == 0
-          result.force_encoding('ASCII-8BIT')
-          File.open(File.expand_path(file_name, image_dir), "w") { |f| f.write result }
-        end
-
-        {:type => :paragraph, :source => "image:#{file_name}[]"}
+        result, status = Open3.capture2e(cmd, :stdin_data => code.encode(Encoding::UTF_8))
+        raise "PlantUML exited with code #{status.exitstatus}" if status.exitstatus != 0
+        result
       end
 
       def file_name(code)
         sha256 = Digest::SHA256.new
-        code.each { |line| sha256 << line }
+        sha256 << code
         sha256.hexdigest
       end
     end
