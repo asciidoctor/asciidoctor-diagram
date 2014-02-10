7af428368e45472d42afe26e1b5637af7df0a8a4
Pepijn Van Eeckhoudt <pepijn@vaneeckhoudt.net>
2014-02-10 23:27:37 +0100

    Restore png and utxt format support Add minimal set of specs

---
+++ spec/test_helper.rb
@@ -0,0 +1,42 @@
+require 'asciidoctor'
+require 'asciidoctor/cli/invoker'
+require_relative '../lib/asciidoctor-plantuml'
+require_relative '../lib/asciidoctor-plantuml/extension'
+
+module Asciidoctor
+  class AbstractBlock
+    def find(&block)
+      blocks.each do |b|
+        if block.call(b)
+          return b
+        end
+
+        if (found_block = b.find(&block))
+          return found_block
+        end
+      end
+      nil
+    end
+  end
+end
+
+RSpec.configure do |c|
+  TEST_DIR = 'testing'
+
+  c.before(:all) do
+    FileUtils.rm_r TEST_DIR if Dir.exists? TEST_DIR
+    FileUtils.mkdir_p TEST_DIR
+  end
+
+  c.around(:each) do |example|
+    test_dir = Dir.mktmpdir 'test', File.expand_path(TEST_DIR)
+
+    old_wd = Dir.pwd
+    Dir.chdir test_dir
+    begin
+      example.run
+    ensure
+      Dir.chdir old_wd
+    end
+  end
+end
