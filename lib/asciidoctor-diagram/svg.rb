6b84bc5a473cbff071f6d59a652cb8a93fe225ac
Pepijn Van Eeckhoudt <pepijn@vaneeckhoudt.net>
2014-02-13 21:13:44 +0100

    Add Graphviz DOT support

--- lib/asciidoctor-diagram/svg.rb
+++ lib/asciidoctor-diagram/svg.rb
@@ -3,14 +3,42 @@ require_relative 'binaryio'
 module Asciidoctor
   module Diagram
     module SVG
-      SVG_SIZE_REGEX = /style="width:(?<width>\d+)px;height:(?<height>\d+)px/
-
       def self.get_image_size(data)
-        match_data = SVG_SIZE_REGEX.match(data)
+        get_from_style(data) || get_from_viewport(data)
+      end
+
+      private
+
+      SVG_STYLE_REGEX = /style\s*=\s*"width:(?<width>\d+)px;height:(?<height>\d+)px/
+
+      def self.get_from_style(data)
+        match_data = SVG_STYLE_REGEX.match(data)
         if match_data
           [match_data[:width].to_i, match_data[:height].to_i]
         else
           nil
+        end
+      end
+
+      SVG_VIEWPORT_REGEX = /<svg width="(?<width>\d+)(?<width_unit>[a-zA-Z]+)" height="(?<height>\d+)(?<height_unit>[a-zA-Z]+)"/
+
+      def self.get_from_viewport(data)
+        match_data = SVG_VIEWPORT_REGEX.match(data)
+        if match_data
+          width = match_data[:width].to_i * to_px_factor(match_data[:width_unit])
+          height = match_data[:height].to_i * to_px_factor(match_data[:height_unit])
+          [width.to_i, height.to_i]
+        else
+          nil
+        end
+      end
+
+      def self.to_px_factor(unit)
+        case unit
+          when 'pt'
+            1.33
+          else
+            1
         end
       end
     end
