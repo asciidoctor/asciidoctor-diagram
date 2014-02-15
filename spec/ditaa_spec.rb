43439a71adb06713965cd1c973cbdaaae0a18b89
Pepijn Van Eeckhoudt <pepijn@vaneeckhoudt.net>
2014-02-15 20:39:37 +0100

    Add block macros for each support diagram format

--- spec/ditaa_spec.rb
+++ spec/ditaa_spec.rb
@@ -1,5 +1,48 @@
 require_relative 'test_helper'
 
+describe Asciidoctor::Diagram::DitaaBlockMacro do
+  it "should generate PNG images when format is set to 'png'" do
+    code = <<-eos
++--------+   +-------+    +-------+
+|        | --+ ditaa +--> |       |
+|  Text  |   +-------+    |diagram|
+|Document|   |!magic!|    |       |
+|     {d}|   |       |    |       |
++---+----+   +-------+    +-------+
+    :                         ^
+    |       Lots of work      |
+    +-------------------------+
+    eos
+
+    File.write('ditaa.txt', code)
+
+    doc = <<-eos
+= Hello, PlantUML!
+Doc Writer <doc@example.com>
+
+== First Section
+
+ditaa::ditaa.txt[format="png"]
+    eos
+
+    d = Asciidoctor.load StringIO.new(doc)
+    expect(d).to_not be_nil
+
+    b = d.find { |b| b.context == :image }
+    expect(b).to_not be_nil
+
+    expect(b.content_model).to eq :empty
+
+    target = b.attributes['target']
+    expect(target).to_not be_nil
+    expect(target).to match /\.png$/
+    expect(File.exists?(target)).to be_true
+
+    expect(b.attributes['width']).to_not be_nil
+    expect(b.attributes['height']).to_not be_nil
+  end
+end
+
 describe Asciidoctor::Diagram::DitaaBlock do
   it "should generate PNG images when format is set to 'png'" do
     doc = <<-eos
--- spec/graphviz_spec.rb
+++ spec/graphviz_spec.rb
@@ -1,6 +1,48 @@
 require_relative 'test_helper'
 
-describe Asciidoctor::Diagram::DitaaBlock do
+describe Asciidoctor::Diagram::GraphvizBlockMacro do
+  it "should generate PNG images when format is set to 'png'" do
+    code = <<-eos
+digraph foo {
+  node [style=rounded]
+  node1 [shape=box]
+  node2 [fillcolor=yellow, style="rounded,filled", shape=diamond]
+  node3 [shape=record, label="{ a | b | c }"]
+
+  node1 -> node2 -> node3
+}
+    eos
+
+    File.write('graphviz.txt', code)
+
+    doc = <<-eos
+= Hello, graphviz!
+Doc Writer <doc@example.com>
+
+== First Section
+
+graphviz::graphviz.txt[format="png"]
+    eos
+
+    d = Asciidoctor.load StringIO.new(doc)
+    expect(d).to_not be_nil
+
+    b = d.find { |b| b.context == :image }
+    expect(b).to_not be_nil
+
+    expect(b.content_model).to eq :empty
+
+    target = b.attributes['target']
+    expect(target).to_not be_nil
+    expect(target).to match /\.png$/
+    expect(File.exists?(target)).to be_true
+
+    expect(b.attributes['width']).to_not be_nil
+    expect(b.attributes['height']).to_not be_nil
+  end
+end
+
+describe Asciidoctor::Diagram::GraphvizBlock do
   it "should generate PNG images when format is set to 'png'" do
     doc = <<-eos
 = Hello, graphviz!
