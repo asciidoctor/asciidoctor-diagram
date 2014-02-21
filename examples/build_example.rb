a738dbbc556156fc8bd7a2bb6161e66c48a94f45
Pepijn Van Eeckhoudt <pepijn@vaneeckhoudt.net>
2014-02-21 23:34:47 +0100

    Update example build instructions for asciidoctor 1.5.0

--- examples/build_example.rb
+++
@@ -1,9 +0,0 @@
-require 'rubygems'
-require 'bundler/setup'
-
-require 'asciidoctor'
-require 'asciidoctor/cli/options'
-require 'asciidoctor/cli/invoker'
-require 'asciidoctor-diagram'
-
-Asciidoctor::Cli::Invoker.new(*ARGV).invoke!
