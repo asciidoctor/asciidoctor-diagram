7182e3559ca5be4ea66bfe2ce4bd70202f8f2c12
Pepijn Van Eeckhoudt <pepijn@vaneeckhoudt.net>
2014-07-15 16:39:36 +0200

    Move which method to separate file

---
+++ lib/asciidoctor-diagram/util/which.rb
@@ -0,0 +1,14 @@
+module Asciidoctor
+  module Diagram
+    def self.which(cmd)
+      exts = ENV['PATHEXT'] ? ENV['PATHEXT'].split(';') : ['']
+      ENV['PATH'].split(File::PATH_SEPARATOR).each do |path|
+        exts.each { |ext|
+          exe = File.join(path, "#{cmd}#{ext}")
+          return exe if File.executable? exe
+        }
+      end
+      nil
+    end
+  end
+end
