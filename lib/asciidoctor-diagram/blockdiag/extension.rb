882a896891222a9a44d137e464a4e3d945dba990
Pepijn Van Eeckhoudt <pepijn@vaneeckhoudt.net>
2014-08-12 21:45:18 +0200

    Add support for Block/Act/Seq/Nwdiag Deduplicate extension code

---
+++ lib/asciidoctor-diagram/blockdiag/extension.rb
@@ -0,0 +1,18 @@
+require_relative '../util/cli_generator'
+require_relative '../util/diagram'
+
+module Asciidoctor
+  module Diagram
+    ['BlockDiag', 'SeqDiag', 'ActDiag', 'NwDiag', 'RackDiag', 'PacketDiag'].each do |tool|
+      DiagramProcessor.define_processors(tool) do
+        [:png, :svg].each do |f|
+          register_format(f, :image) do |c, p|
+            CliGenerator.generate(tool.downcase, p, c) do |tool_path, output_path|
+              [tool_path, '-o', output_path, "-T#{f.to_s}", '-']
+            end
+          end
+        end
+      end
+    end
+  end
+end
