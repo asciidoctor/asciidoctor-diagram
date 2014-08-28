aba68e51806f411e4542f950b6550f17cb370ef5
Pepijn Van Eeckhoudt <pepijn@vaneeckhoudt.net>
2014-08-28 20:15:09 +0200

    More documentation

--- lib/asciidoctor-diagram/extensions.rb
+++ lib/asciidoctor-diagram/extensions.rb
@@ -113,7 +113,7 @@ module Asciidoctor
         #        target value of a block macro
         # @param attributes [Hash] the attributes of the block or block macro
         #
-        # @return [DiagramSource]
+        # @return [DiagramSource] an object that implements the interface described by DiagramSource
         #
         # @abstract
         def create_source(parent, reader_or_target, attributes)
@@ -211,30 +211,48 @@ module Asciidoctor
         end
       end
 
-      #
+      # This module describes the duck-typed interface that diagram sources must implement. Implementations
+      # may include this module but it is not required.
       module DiagramSource
+        # @return [String] the base name for the image file that will be produced
+        # @abstract
         def image_name
           raise NotImplementedError.new
         end
 
+        # @return [String] the String representation of the source code for the diagram
+        # @abstract
         def code
           raise NotImplementedError.new
         end
 
+        # Alias for code
         def to_s
           code
         end
 
+        # Determines if the diagram should be regenerated or not. The default implementation of this method simply
+        # returns true.
+        #
+        # @param image_file [String] the path to the previously generated version of the image
+        # @param image_metadata [Hash] the image metadata Hash that was stored during the previous diagram generation pass
+        # @return [Boolean] true if the diagram should be regenerated; false otherwise
         def should_process?(image_file, image_metadata)
           true
         end
 
+        # Creates an image metadata Hash that will be stored to disk alongside the generated image file. The contents
+        # of this Hash are reread during subsequent document processing and then passed to the should_process? method
+        # where it can be used to determine if the diagram should be regenerated or not.
+        # The default implementation returns an empty Hash.
+        # @return [Hash] a Hash containing metadata
         def create_image_metadata
           {}
         end
       end
 
-      # Public: Base c
+      # Base class for diagram source implementations that uses an md5 checksum of the source code of a diagram to
+      # determine if it has been updated or not.
       class BasicSource
         include DiagramSource
 
@@ -265,6 +283,7 @@ module Asciidoctor
         end
       end
 
+      # A diagram source that retrieves the code for the diagram from the contents of a block.
       class ReaderSource < BasicSource
         include DiagramSource
 
@@ -278,6 +297,7 @@ module Asciidoctor
         end
       end
 
+      # A diagram source that retrieves the code for a diagram from an external source file.
       class FileSource < BasicSource
         def initialize(file_name, attributes)
           super(attributes)
