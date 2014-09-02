fb2c8df8710995c787c072bf399f9b09ee14d472
Pepijn Van Eeckhoudt <pepijn@vaneeckhoudt.net>
2014-09-02 20:06:16 +0200

    Correctly register SeqDiag, ActDiag, NwDiag, RackDiag and PacketDiag processors

--- lib/asciidoctor-diagram/blockdiag.rb
+++ lib/asciidoctor-diagram/blockdiag.rb
@@ -3,6 +3,13 @@ require_relative 'version'
 
 Asciidoctor::Extensions.register do
   require_relative 'blockdiag/extension'
-  block Asciidoctor::Diagram::BlockDiagBlock, :blockdiag
-  block_macro Asciidoctor::Diagram::BlockDiagBlockMacro, :blockdiag
+
+  ['BlockDiag', 'SeqDiag', 'ActDiag', 'NwDiag', 'RackDiag', 'PacketDiag'].each do |tool|
+    name = tool.downcase.to_sym
+    block = Asciidoctor::Diagram.const_get("#{tool}Block")
+    block_macro = Asciidoctor::Diagram.const_get("#{tool}BlockMacro")
+
+    block block, name
+    block_macro block_macro, name
+  end
 end
