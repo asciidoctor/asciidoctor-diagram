2430d8a330f3b8aee4a66ef5e0d5bef314d0383f
Filipe Alves <filipe.alves@bithium.com>
2016-08-10 12:17:41 +0100

    Pass blockdiag types images thru anti-alias filter.

--- lib/asciidoctor-diagram/blockdiag/extension.rb
+++ lib/asciidoctor-diagram/blockdiag/extension.rb
@@ -106,7 +106,7 @@ module Asciidoctor
         alt_cmd_name = "#{tool.downcase}3"
 
         CliGenerator.generate_stdin(which(parent, cmd_name, :alt_cmds => [alt_cmd_name]), format.to_s, source.to_s) do |tool_path, output_path|
-          args = [tool_path, '-o', Platform.native_path(output_path), "-T#{format.to_s}"]
+          args = [tool_path, '-a', -o', Platform.native_path(output_path), "-T#{format.to_s}"]
           args << "-f#{Platform.native_path(parent.document.attributes['blockdiag-font'])}" if parent.document.attributes.key?('blockdiag-font')
           args << "-"
           args
