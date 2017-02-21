bddfe331fe4a1f9b0f3ad881063c20756e4a2c6e
Brent Douglas <brent.n.douglas@gmail.com>
2017-02-21 12:26:52 +0100

    Allow execution when ENV['PATH'] is not defined
    The PATH environment variable is not required however this code throws if it is not provided.

--- lib/asciidoctor-diagram/util/which.rb
+++ lib/asciidoctor-diagram/util/which.rb
@@ -5,7 +5,7 @@ module Asciidoctor
       def self.which(cmd, options = {})
         exts = ENV['PATHEXT'] ? ENV['PATHEXT'].split(';') : ['']
 
-        paths = (options[:path] || []) + ENV['PATH'].split(File::PATH_SEPARATOR)
+        paths = (options[:path] || []) + (ENV['PATH'] ? ENV['PATH'].split(File::PATH_SEPARATOR) : [])
         paths.each do |path|
           exts.each { |ext|
             exe = File.join(path, "#{cmd}#{ext}")
