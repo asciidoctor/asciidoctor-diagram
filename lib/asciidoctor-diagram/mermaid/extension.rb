d107bbe1a85a806c97902261ec80a157ee2208fa
Pepijn Van Eeckhoudt <pepijn@vaneeckhoudt.net>
2018-02-24 12:13:51 +0100

    #163 Add support for mermaid.cli (mmdc)

--- lib/asciidoctor-diagram/mermaid/extension.rb
+++ lib/asciidoctor-diagram/mermaid/extension.rb
@@ -21,27 +21,97 @@ module Asciidoctor
 
       def mermaid(parent_block, source, format)
         inherit_prefix = name
-        mermaid = which(parent_block, 'mermaid')
-        config['mermaid>=6'] ||= ::Asciidoctor::Diagram::Cli.run(mermaid, '--version').split('.')[0].to_i >= 6
-        # Mermaid >= 6.0.0 requires PhantomJS 2.1; older version required 1.9
-        phantomjs = which(parent_block, 'phantomjs', :alt_attrs => [config['mermaid>=6'] ? 'phantomjs_2' : 'phantomjs_19'])
+
+        options = {}
 
         css = source.attr('css', nil, inherit_prefix)
         if css
-          css = parent_block.normalize_system_path(css, source.base_dir)
+          options[:css] = parent_block.normalize_system_path(css, source.base_dir)
         end
 
         gantt_config = source.attr('ganttConfig', nil, inherit_prefix) || source.attr('ganttconfig', nil, inherit_prefix)
         if gantt_config
-          gantt_config = parent_block.normalize_system_path(gantt_config, source.base_dir)
+          options[:gantt] = parent_block.normalize_system_path(gantt_config, source.base_dir)
         end
 
         seq_config = source.attr('sequenceConfig', nil, inherit_prefix) || source.attr('sequenceconfig', nil, inherit_prefix)
         if seq_config
-          seq_config = parent_block.normalize_system_path(seq_config, source.base_dir)
+          options[:sequence] = parent_block.normalize_system_path(seq_config, source.base_dir)
         end
 
-        width = source.attr('width', nil, inherit_prefix)
+        options[:width] = source.attr('width', nil, inherit_prefix)
+
+        mmdc = which(parent_block, 'mmdc', :raise_on_error => false)
+        if mmdc
+          options[:height] = source.attr('height', nil, inherit_prefix)
+          options[:theme] = source.attr('theme', nil, inherit_prefix)
+          options[:background] = source.attr('background', nil, inherit_prefix)
+          config = source.attr('config', nil, inherit_prefix) || source.attr('config', nil, inherit_prefix)
+          if config
+            options[:config] = parent_block.normalize_system_path(config, source.base_dir)
+          end
+          run_mmdc(mmdc, source, format, options)
+        else
+          mermaid = which(parent_block, 'mermaid')
+          run_mermaid(mermaid, parent_block, source, format, options)
+        end
+      end
+
+      private
+      def run_mmdc(mmdc, source, format, options = {})
+        generate_file(mmdc, 'mmd', format.to_s, source.to_s) do |tool_path, input_path, output_path|
+          args = [tool_path, '-i', Platform.native_path(input_path), '-o', Platform.native_path(output_path)]
+
+          if options[:css]
+            args << '--cssFile' << Platform.native_path(options[:css])
+          end
+
+          if options[:theme]
+            args << '--theme' << options[:theme]
+          end
+
+          if options[:width]
+            args << '--width' << options[:width]
+          end
+
+          if options[:height]
+            args << '--height' << options[:height]
+          end
+
+          if options[:background]
+            bg = options[:background]
+            bg = "##{bg}" unless bg[0] == '#'
+            args << '--backgroundColor' << bg
+          end
+
+          if options[:config]
+            args << '--configFile' << Platform.native_path(options[:config])
+          elsif options[:gantt] || options[:sequence]
+            config = []
+
+            if options[:gantt]
+              config << "\"gantt\": #{File.read(options[:gantt])}"
+            end
+
+            if options[:sequence]
+              config << "\"sequenceDiagram\": #{File.read(options[:sequence])}"
+            end
+
+            config_file = "#{input_path}.json"
+
+            File.write(config_file, "{#{config.join ','}}")
+
+            args << '--configFile' << Platform.native_path(config_file)
+          end
+
+          args
+        end
+      end
+
+      def run_mermaid(mermaid, parent_block, source, format, options = {})
+        config['mermaid>=6'] ||= ::Asciidoctor::Diagram::Cli.run(mermaid, '--version').split('.')[0].to_i >= 6
+        # Mermaid >= 6.0.0 requires PhantomJS 2.1; older version required 1.9
+        phantomjs = which(parent_block, 'phantomjs', :alt_attrs => [config['mermaid>=6'] ? 'phantomjs_2' : 'phantomjs_19'])
 
         generate_file(mermaid, 'mmd', format.to_s, source.to_s) do |tool_path, input_path, output_path|
           output_dir = File.dirname(output_path)
@@ -49,20 +119,20 @@ module Asciidoctor
 
           args = [tool_path, '--phantomPath', Platform.native_path(phantomjs), "--#{format.to_s}", '-o', Platform.native_path(output_dir)]
 
-          if css
-            args << '--css' << Platform.native_path(css)
+          if options[:css]
+            args << '--css' << Platform.native_path(options[:css])
           end
 
-          if gantt_config
-            args << '--gantt_config' << Platform.native_path(gantt_config)
+          if options[:gantt]
+            args << '--gantt_config' << Platform.native_path(options[:gantt])
           end
 
-          if seq_config
-            args << '--sequenceConfig' << Platform.native_path(seq_config)
+          if options[:sequence]
+            args << '--sequenceConfig' << Platform.native_path(options[:sequence])
           end
 
-          if width
-            args << '--width' << width
+          if options[:width]
+            args << '--width' << options[:width]
           end
 
           args << Platform.native_path(input_path)
