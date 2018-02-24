a1aea68f3a37d2a14e814c627a82ba55a2c11a74
Pepijn Van Eeckhoudt <pepijn@vaneeckhoudt.net>
2018-02-24 16:40:49 +0100

    Correct process exit code check

--- lib/asciidoctor-diagram/util/cli.rb
+++ lib/asciidoctor-diagram/util/cli.rb
@@ -8,7 +8,7 @@ module Asciidoctor
       def self.run(*args)
         stdout, stderr, status = Open3.capture3(*args)
 
-        if status != 0
+        if status.exitstatus != 0
           raise "#{File.basename(args[0])} failed: #{stdout.empty? ? stderr : stdout}"
         end
 
