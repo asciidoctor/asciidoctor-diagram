a3c0b2a8208aa0880809b91b9bb267b9177283b0
Pepijn Van Eeckhoudt <pepijn@vaneeckhoudt.net>
2019-02-10 21:50:30 +0100

    Svgbob converter

--- lib/asciidoctor-diagram/svgbob/extension.rb
+++ lib/asciidoctor-diagram/svgbob/extension.rb
@@ -1,34 +1,14 @@
+require_relative 'converter'
 require_relative '../extensions'
-require_relative '../util/cli_generator'
-require_relative '../util/platform'
 
 module Asciidoctor
   module Diagram
-    # @private
-    module Svgbob
-      include CliGenerator
-
-      def self.included(mod)
-        [:svg].each do |f|
-          mod.register_format(f, :image) do |parent, source|
-            svgbob(source, f)
-          end
-        end
-      end
-
-      def svgbob(source, format)
-        generate_stdin(source.find_command('svgbob'), format.to_s, source.to_s) do |tool_path, output_path|
-          [tool_path, '-o', Platform.native_path(output_path)]
-        end
-      end
-    end
-
     class SvgBobBlockProcessor < Extensions::DiagramBlockProcessor
-      include Svgbob
+      use_converter SvgbobConverter
     end
 
     class SvgBobBlockMacroProcessor < Extensions::DiagramBlockMacroProcessor
-      include Svgbob
+      use_converter SvgbobConverter
     end
   end
 end
