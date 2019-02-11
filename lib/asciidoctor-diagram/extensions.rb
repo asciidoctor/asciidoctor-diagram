36f34c29ba98b091025b6cbc1771e93e9616226e
Pepijn Van Eeckhoudt <pepijn@vaneeckhoudt.net>
2019-02-11 09:21:59 +0100

    Roll back workaround for asciidoctor#3059

--- lib/asciidoctor-diagram/extensions.rb
+++ lib/asciidoctor-diagram/extensions.rb
@@ -418,9 +418,9 @@ module Asciidoctor
           if value.nil? && inherit
             case inherit
               when String, Symbol
-                value = @parent_block.attr("#{inherit.to_s}-#{name}", default_value)
+                value = @parent_block.attr("#{inherit.to_s}-#{name}", default_value, true)
               else
-                value = @parent_block.attr(name, default_value)
+                value = @parent_block.attr(name, default_value, true)
             end
           end
 
