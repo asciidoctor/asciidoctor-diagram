eec819ff083b2da97ec90cc775f79630419148c9
Pepijn Van Eeckhoudt <pepijn@vaneeckhoudt.net>
2019-02-18 22:18:34 +0100

    Syntrax converter

--- lib/asciidoctor-diagram/syntrax/extension.rb
+++ lib/asciidoctor-diagram/syntrax/extension.rb
@@ -1,57 +1,14 @@
 require_relative '../extensions'
-require_relative '../util/cli_generator'
-require_relative '../util/platform'
+require_relative 'converter'
 
 module Asciidoctor
   module Diagram
-    # @private
-    module Syntrax
-      include CliGenerator
-
-      def self.included(mod)
-        [:png, :svg].each do |f|
-          mod.register_format(f, :image) do |parent, source|
-            syntrax(source, f)
-          end
-        end
-      end
-
-      def syntrax(source, format)
-        inherit_prefix = name
-
-        generate_file(source.find_command('syntrax'), 'spec', format.to_s, source.to_s) do |tool_path, input_path, output_path|
-          args = [tool_path, '-i', Platform.native_path(input_path), '-o', Platform.native_path(output_path)]
-
-          title = source.attr('heading', nil, inherit_prefix)
-          if title
-            args << '--title' << title
-          end
-
-          scale = source.attr('scale', nil, inherit_prefix)
-          if scale
-            args << '--scale' << scale
-          end
-
-          transparent = source.attr('transparent', nil, inherit_prefix)
-          if transparent == 'true'
-            args << '--transparent'
-          end
-          style = source.attr('style', nil, inherit_prefix)
-          if style
-            args << '--style' << style
-          end
-
-          args
-        end
-      end
-    end
-
     class SyntraxBlockProcessor < Extensions::DiagramBlockProcessor
-      include Syntrax
+      use_converter SyntraxConverter
     end
 
     class SyntraxBlockMacroProcessor < Extensions::DiagramBlockMacroProcessor
-      include Syntrax
+      use_converter SyntraxConverter
     end
   end
 end
