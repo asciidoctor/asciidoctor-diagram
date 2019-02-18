a1625e8f3b72d9b0b0e6954e81243aeb4481601f
Pepijn Van Eeckhoudt <pepijn@vaneeckhoudt.net>
2019-02-18 22:31:27 +0100

    Vega converter

--- lib/asciidoctor-diagram/vega/extension.rb
+++ lib/asciidoctor-diagram/vega/extension.rb
@@ -1,50 +1,14 @@
 require_relative '../extensions'
-require_relative '../util/cli_generator'
-require_relative '../util/platform'
+require_relative 'converter'
 
 module Asciidoctor
   module Diagram
-    # @private
-    module Vega
-      include CliGenerator
-
-      def self.included(mod)
-        [:svg, :png].each do |f|
-          mod.register_format(f, :image) do |parent, source|
-            vega(source, f)
-          end
-        end
-      end
-
-      def vega(source, format)
-        base_dir = source.base_dir
-
-        code = source.to_s
-
-        if code.include?('/schema/vega-lite/') || name.to_s.include?('lite') || source.attr('vegalite')
-          vega_code = generate_stdin_stdout(source.find_command("vl2vg"), code)
-        else
-          vega_code = code
-        end
-
-        generate_file(source.find_command("vg2#{format}"), "json", format.to_s, vega_code) do |tool_path, input_path, output_path|
-          args = [tool_path, '--base', Platform.native_path(base_dir)]
-          if format == :svg
-            args << '--header'
-          end
-
-          args << Platform.native_path(input_path)
-          args << Platform.native_path(output_path)
-        end
-      end
-    end
-
     class VegaBlockProcessor < Extensions::DiagramBlockProcessor
-      include Vega
+      use_converter VegaConverter
     end
 
     class VegaBlockMacroProcessor < Extensions::DiagramBlockMacroProcessor
-      include Vega
+      use_converter VegaConverter
     end
   end
 end
