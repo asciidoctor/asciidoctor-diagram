f6954889a12d2c58c709a94254aaa0cc1663d4f6
Pepijn Van Eeckhoudt <pepijn@vaneeckhoudt.net>
2019-06-12 09:15:07 +0200

    #225 Add support for WaveDrom CLI 2.0.0

--- lib/asciidoctor-diagram/wavedrom/extension.rb
+++ lib/asciidoctor-diagram/wavedrom/extension.rb
@@ -19,25 +19,33 @@ module Asciidoctor
       end
 
       def wavedrom(parent, source, format)
-        wavedrom_cli = which(parent, 'wavedrom', :raise_on_error => false)
-        phantomjs = which(parent, 'phantomjs', :alt_attrs => ['phantomjs_2'], :raise_on_error => false)
+        wavedrom_cli = which(parent, 'wavedrom-cli', :raise_on_error => false)
 
-        if wavedrom_cli && !wavedrom_cli.include?('WaveDromEditor') && phantomjs
+        if wavedrom_cli
           generate_file(wavedrom_cli, 'wvd', format.to_s, source.to_s) do |tool_path, input_path, output_path|
-            [phantomjs, Platform.native_path(tool_path), '-i', Platform.native_path(input_path), "-#{format.to_s[0]}", Platform.native_path(output_path)]
+            [Platform.native_path(tool_path), '--input', Platform.native_path(input_path), "--#{format.to_s}", Platform.native_path(output_path)]
           end
         else
-          if ::Asciidoctor::Diagram::Platform.os == :macosx
-            wavedrom = which(parent, 'WaveDromEditor.app', :alt_cmds => ['wavedrom-editor.app'], :attrs => ['WaveDromEditorApp'],:path => ['/Applications'])
-            if wavedrom
-              wavedrom = File.join(wavedrom, 'Contents/MacOS/nwjs')
+          wavedrom_cli = which(parent, 'wavedrom', :raise_on_error => false)
+          phantomjs = which(parent, 'phantomjs', :alt_attrs => ['phantomjs_2'], :raise_on_error => false)
+
+          if wavedrom_cli && !wavedrom_cli.include?('WaveDromEditor') && phantomjs
+            generate_file(wavedrom_cli, 'wvd', format.to_s, source.to_s) do |tool_path, input_path, output_path|
+              [phantomjs, Platform.native_path(tool_path), '-i', Platform.native_path(input_path), "-#{format.to_s[0]}", Platform.native_path(output_path)]
             end
           else
-            wavedrom = which(parent, 'WaveDromEditor')
-          end
+            if ::Asciidoctor::Diagram::Platform.os == :macosx
+              wavedrom = which(parent, 'WaveDromEditor.app', :alt_cmds => ['wavedrom-editor.app'], :attrs => ['WaveDromEditorApp'],:path => ['/Applications'])
+              if wavedrom
+                wavedrom = File.join(wavedrom, 'Contents/MacOS/nwjs')
+              end
+            else
+              wavedrom = which(parent, 'WaveDromEditor')
+            end
 
-          generate_file(wavedrom, 'wvd', format.to_s, source.to_s) do |tool_path, input_path, output_path|
-            [tool_path, 'source', Platform.native_path(input_path), format.to_s, Platform.native_path(output_path)]
+            generate_file(wavedrom, 'wvd', format.to_s, source.to_s) do |tool_path, input_path, output_path|
+              [tool_path, 'source', Platform.native_path(input_path), format.to_s, Platform.native_path(output_path)]
+            end
           end
         end
       end
