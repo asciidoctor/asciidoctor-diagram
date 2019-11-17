94ce95b241d4fc828168bcc9235c84a780e114f0
Pepijn Van Eeckhoudt <pepijn@vaneeckhoudt.net>
2019-11-17 18:40:27 +0100

    #231 Fix unit test

--- spec/plantuml_spec.rb
+++ spec/plantuml_spec.rb
@@ -1088,20 +1088,8 @@ Bob; Alice; foo
 ----
     eos
 
-    d = load_asciidoc doc
-    expect(d).to_not be_nil
-
-    b = d.find { |bl| bl.context == :image }
-    expect(b).to_not be_nil
-
-    expect(b.content_model).to eq :empty
-
-    target = b.attributes['target']
-    expect(target).to_not be_nil
-    expect(target).to match(/\.svg$/)
-    expect(File.exist?(target)).to be true
-
-    expect(b.attributes['width']).to_not be_nil
-    expect(b.attributes['height']).to_not be_nil
+    expect {
+      load_asciidoc doc
+    }.to raise_error(/syntax error/i)
   end
 end
