c59ec1ec0cc5ff1f2bfc5c8d312ed705870b56ee
Pepijn Van Eeckhoudt <pepijn@vaneeckhoudt.net>
2019-11-17 16:13:43 +0100

    #231 Improve PlantUML error reporting

--- lib/asciidoctor-diagram/plantuml/converter.rb
+++ lib/asciidoctor-diagram/plantuml/converter.rb
@@ -8,7 +8,7 @@ module Asciidoctor
       include DiagramConverter
 
       JARS = [
-          'plantuml-1.3.13.jar',
+          'plantuml-1.3.14.jar',
           'plantuml.jar',
           'jlatexmath-minimal-1.0.5.jar',
           'batik-all-1.10.jar'
