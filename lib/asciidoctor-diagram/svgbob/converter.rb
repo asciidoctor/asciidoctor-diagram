f5f2c9ea1b56bfb07370d4538944d1abee8af541
hackingotter <hackingotter@users.noreply.github.com>
2020-04-13 06:14:05 -0400

    add options for svgbob (#265)
    Co-authored-by: l.ott <l.ott@codesys.com>

--- lib/asciidoctor-diagram/svgbob/converter.rb
+++ lib/asciidoctor-diagram/svgbob/converter.rb
@@ -14,10 +14,34 @@ module Asciidoctor
         [:svg]
       end
 
+      OPTIONS = {
+          :font_family => lambda { |o, v| o << '--font-family' << v if v },
+          :font_size => lambda { |o, v| o << '--font-size' << v if v },
+          :stroke_width => lambda { |o, v| o << '--stroke-width' << v if v },
+          :scale => lambda { |o, v| o << '--scale' << v if v }
+      }
+
+      def collect_options(source, name)
+        options = {}
+
+        OPTIONS.keys.each do |option|
+          attr_name = option.to_s.tr('_', '-')
+          options[option] = source.attr(attr_name, nil, name) || source.attr(attr_name, nil, 'svgbob-option')
+        end
+
+        options
+      end
+
 
       def convert(source, format, options)
+
+        flags = []
+        options.each do |option, value|
+          OPTIONS[option].call(flags, value)
+        end
+        
         generate_stdin(source.find_command('svgbob'), format.to_s, source.to_s) do |tool_path, output_path|
-          [tool_path, '-o', Platform.native_path(output_path)]
+          ([tool_path, '-o', Platform.native_path(output_path)] + flags)
         end
       end
     end
