bfb13eecf241bbc5352b8bd6b397580823160642
Christopher Brown <chribrow@redhat.com>
2021-01-13 12:31:53 +0000

    Enable various tests on Mac OS

--- spec/lilypond_spec.rb
+++ spec/lilypond_spec.rb
@@ -4,10 +4,10 @@ code = <<-eos
 \\relative c' { f d f a d f e d cis a cis e a g f e }
 eos
 
-describe Asciidoctor::Diagram::LilypondBlockMacroProcessor, :broken_on_windows, :broken_on_osx do
+describe Asciidoctor::Diagram::LilypondBlockMacroProcessor, :broken_on_osx, :broken_on_windows do
   include_examples "block_macro", :lilypond, code, [:png]
 end
 
-describe Asciidoctor::Diagram::LilypondBlockProcessor, :broken_on_windows, :broken_on_osx do
+describe Asciidoctor::Diagram::LilypondBlockProcessor, :broken_on_osx, :broken_on_windows do
   include_examples "block", :lilypond, code, [:png]
 end
