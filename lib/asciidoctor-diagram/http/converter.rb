e0c1026af58aab85aeb043fe7428443f375c7c78
Pepijn Van Eeckhoudt <pepijn@vaneeckhoudt.net>
2021-03-29 16:19:49 +0200

    #345 Use POST requests when URI length exceeds a configurable limit

--- lib/asciidoctor-diagram/http/converter.rb
+++ lib/asciidoctor-diagram/http/converter.rb
@@ -11,6 +11,8 @@ module Asciidoctor
   module Diagram
     # @private
     class HttpConverter
+      DEFAULT_MAX_GET_SIZE = 1024
+
       include DiagramConverter
 
       def initialize(base_uri, type, converter)
@@ -21,6 +23,14 @@ module Asciidoctor
 
       def supported_formats
         @converter.supported_formats
+      end
+
+      def collect_options(source)
+        options = {}
+
+        options[:max_get_size] = source.global_attr('max-get-size', DEFAULT_MAX_GET_SIZE).to_i
+
+        options
       end
 
       def convert(source, format, options)
@@ -41,37 +51,49 @@ module Asciidoctor
           encoded = Base64.urlsafe_encode64(compressed)
           data = '0A' + encoded
 
-          path = uri.path
+          path = uri.path.dup
           path << '/' unless path.end_with? '/'
           path << format.to_s
-          path << '/' << data
-          uri.path = path
         when :kroki_io
           compressed = Zlib.deflate(code, Zlib::BEST_COMPRESSION)
           data = Base64.urlsafe_encode64(compressed)
 
-          path = uri.path
+          path = uri.path.dup
           path << '/' unless path.end_with? '/'
           path << source.diagram_type.to_s
           path << '/' << format.to_s
-          path << '/' << data
-          uri.path = path
         else
           raise "Unsupported server type: " + @type
         end
 
-        get_uri(uri)
+        get_path = path.dup << '/' << data
+
+        if get_path.length > options[:max_get_size]
+          uri.path = path
+          get_uri(uri, code, 'text/plain; charset=utf-8')
+        else
+          uri.path = get_path
+          get_uri(uri)
+        end
       end
 
       private
 
-      def get_uri(uri, attempt = 1)
+      def get_uri(uri, post_data = nil, post_content_type = nil, attempt = 1)
         if attempt >= 10
           raise "Too many redirects"
         end
 
+        if post_data.nil?
+          request = Net::HTTP::Get.new(uri.path)
+        else
+          request = Net::HTTP::Post.new(uri.path)
+          request.body = post_data
+          request.set_content_type(post_content_type) if post_content_type
+        end
+
         Net::HTTP.start(uri.hostname, uri.port, :use_ssl => uri.scheme.downcase == 'https') do |http|
-          response = http.request_get uri.path
+          response = http.request(request)
           case response
             when Net::HTTPSuccess
               response.body
@@ -79,10 +101,12 @@ module Asciidoctor
               location = response['Location']
               new_uri = URI.parse(location)
               if new_uri.relative?
-                get_uri(uri + location, attempt + 1)
+                resolved_uri = uri + location
               else
-                get_uri(new_uri, attempt + 1)
+                resolved_uri = new_uri
               end
+
+              get_uri(resolved_uri, post_data, post_content_type, attempt + 1)
             else
               response.value
           end
