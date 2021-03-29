f00851e1c6f02728fb97f47678ccecd9390eaca9
Pepijn Van Eeckhoudt <pepijn@vaneeckhoudt.net>
2021-03-29 16:19:49 +0200

    #345 Use POST requests when URI length exceeds a configurable limit

--- spec/plantuml_spec.rb
+++ spec/plantuml_spec.rb
@@ -158,6 +158,38 @@ end
 describe Asciidoctor::Diagram::PlantUmlBlockProcessor do
   include_examples "block", :plantuml, code, [:png, :svg, :txt]
 
+  it "should work with plantuml.com" do
+    doc = <<-eos
+= Hello, kroki!
+:diagram-server-url: http://plantuml.com/plantuml
+:diagram-server-type: plantuml
+Doc Writer <doc@example.com>
+
+== First Section
+
+[plantuml, format=svg]
+----
+#{code}
+----
+    eos
+
+    d = load_asciidoc doc
+    expect(d).to_not be_nil
+
+    b = d.find { |bl| bl.context == :image }
+    expect(b).to_not be_nil
+
+    expect(b.content_model).to eq :empty
+
+    target = b.attributes['target']
+    expect(target).to_not be_nil
+    expect(target).to match(/\.svg$/)
+    expect(File.exist?(target)).to be true
+
+    expect(b.attributes['width']).to_not be_nil
+    expect(b.attributes['height']).to_not be_nil
+  end
+
   it 'should use plantuml configuration when specified as a document attribute' do
     doc = <<-eos
 = Hello, PlantUML!
