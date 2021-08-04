674486c91acbb10acc40cba093fd3ba1a5522bef
Pepijn Van Eeckhoudt <pepijn@vaneeckhoudt.net>
2021-08-04 16:41:53 +0200

    Add support for inline barcode macros

--- spec/barcode_spec.rb
+++ spec/barcode_spec.rb
@@ -29,7 +29,7 @@ Doc Writer <doc@example.com>
 
 == First Section
 
-[barcode,test,png,#{type},xdim=1,margin=10,height=100,foreground=darkgray,background=azure]
+[barcode,#{type},test,png,xdim=1,margin=10,height=100,foreground=darkgray,background=azure]
 ----
 #{code}
 ----
@@ -51,6 +51,60 @@ Doc Writer <doc@example.com>
       expect(b.attributes['width']).to_not be_nil
       expect(b.attributes['height']).to_not be_nil
     end
+  end
+
+  it "should support complex QR codes" do
+    doc = <<-eos
+= Hello, barby!
+Doc Writer <doc@example.com>
+
+== First Section
+
+[barcode,qr]
+----
+BEGIN:VCARD
+VERSION:3.0
+N:Doe;John;;;
+FN:John Doe
+ORG:Example.com Inc.;
+TITLE:Imaginary test person
+EMAIL;type=INTERNET;type=WORK;type=pref:johnDoe@example.org
+TEL;type=WORK;type=pref:+1 617 555 1212
+TEL;type=WORK:+1 (617) 555-1234
+TEL;type=CELL:+1 781 555 1212
+TEL;type=HOME:+1 202 555 1212
+item1.ADR;type=WORK:;;2 Enterprise Avenue;Worktown;NY;01111;USA
+item1.X-ABADR:us
+item2.ADR;type=HOME;type=pref:;;3 Acacia Avenue;Hoemtown;MA;02222;USA
+item2.X-ABADR:us
+NOTE:John Doe has a long and varied history\, being documented on more police files that anyone else. Reports of his death are alas numerous.
+item3.URL;type=pref:http\://www.example/com/doe
+item3.X-ABLabel:_$!<HomePage>!$_
+item4.URL:http\://www.example.com/Joe/foaf.df
+item4.X-ABLabel:FOAF
+item5.X-ABRELATEDNAMES;type=pref:Jane Doe
+item5.X-ABLabel:_$!<Friend>!$_
+CATEGORIES:Work,Test group
+X-ABUID:5AD380FD-B2DE-4261-BA99-DE1D1DB52FBE\:ABPerson
+END:VCARD
+----
+    eos
+
+    d = load_asciidoc doc
+    expect(d).to_not be_nil
+
+    b = d.find { |bl| bl.context == :image }
+    expect(b).to_not be_nil
+
+    expect(b.content_model).to eq :empty
+
+    target = b.attributes['target']
+    expect(target).to_not be_nil
+    expect(target).to match(/\.png$/)
+    expect(File.exist?(target)).to be true
+
+    expect(b.attributes['width']).to_not be_nil
+    expect(b.attributes['height']).to_not be_nil
   end
 end
 
@@ -81,6 +135,43 @@ barcode::#{type}[#{code},xdim=1,margin=10,height=100,foreground=darkgray,backgro
 
       expect(b.attributes['width']).to_not be_nil
       expect(b.attributes['height']).to_not be_nil
+    end
+  end
+end
+
+describe Asciidoctor::Diagram::BarcodeInlineMacroProcessor do
+  BARCODES.each_pair do |type, code|
+    it "should support #{type}" do
+      doc = <<-eos
+= Hello, barby!
+Doc Writer <doc@example.com>
+
+== First Section
+
+barcode:#{type}[#{code},xdim=1,margin=10,height=100,foreground=darkgray,background=azure]
+      eos
+
+      d = load_asciidoc doc
+      expect(d).to_not be_nil
+
+      b = d.find { |bl| bl.context == :paragraph }
+      expect(b).to_not be_nil
+
+      output = b.convert
+      img_match = /<img[^>]*>/.match(output)
+      expect(img_match).to_not be_nil
+      img = img_match.to_s
+
+      src_match = /src="([^"]*)"/.match(img)
+      expect(src_match).to_not be_nil
+      src = src_match[1]
+
+      expect(src).to_not be_nil
+      expect(src).to match(/\.png$/)
+      expect(File.exist?(src)).to be true
+
+      expect(/width="([^"]*)"/.match(img)).to_not be_nil
+      expect(/height="([^"]*)"/.match(img)).to_not be_nil
     end
   end
 end
