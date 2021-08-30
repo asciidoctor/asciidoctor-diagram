c3ef8e0128e2d9d160cae14bad2531508a887a2c
Pepijn Van Eeckhoudt <pepijn@vaneeckhoudt.net>
2021-08-30 23:03:59 +0200

    #367 Relax rqrcode and chunky_png version constraints

--- lib/asciidoctor-diagram/barcode/dependencies.rb
+++ lib/asciidoctor-diagram/barcode/dependencies.rb
@@ -2,8 +2,8 @@ module Asciidoctor
   module Diagram
     module BarcodeDependencies
       BARCODE_DEPENDENCIES = {'barby' => '~> 0.6.8'}
-      PNG_DEPENDENCIES = {'chunky_png' => '~> 1.4.0'}
-      QRCODE_DEPENDENCIES = {'rqrcode' => '~> 2.0.0'}
+      PNG_DEPENDENCIES = {'chunky_png' => '~> 1.4'}
+      QRCODE_DEPENDENCIES = {'rqrcode' => '~> 2.0'}
       ALL_DEPENDENCIES = {}.merge(BARCODE_DEPENDENCIES).merge(PNG_DEPENDENCIES).merge(QRCODE_DEPENDENCIES)
     end
   end
