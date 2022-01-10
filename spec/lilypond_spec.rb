fa90596d1333c775c9d63aabfadb60d296c96fea
Pepijn Van Eeckhoudt <pepijn.vaneeckhoudt@datadobi.com>
2022-01-10 10:40:26 +0100

    Disable lilypond tests on Windows; chocolatey installer seems unreliable

--- spec/lilypond_spec.rb
+++ spec/lilypond_spec.rb
@@ -4,14 +4,14 @@ LILYPOND_CODE = <<-eos
 \\relative c' { f d f a d f e d cis a cis e a g f e }
 eos
 
-describe Asciidoctor::Diagram::LilypondInlineMacroProcessor do
+describe Asciidoctor::Diagram::LilypondInlineMacroProcessor, :broken_on_windows do
   include_examples "inline_macro", :lilypond, LILYPOND_CODE, [:png]
 end
 
-describe Asciidoctor::Diagram::LilypondBlockMacroProcessor do
+describe Asciidoctor::Diagram::LilypondBlockMacroProcessor, :broken_on_windows do
   include_examples "block_macro", :lilypond, LILYPOND_CODE, [:png]
 end
 
-describe Asciidoctor::Diagram::LilypondBlockProcessor do
+describe Asciidoctor::Diagram::LilypondBlockProcessor, :broken_on_windows do
   include_examples "block", :lilypond, LILYPOND_CODE, [:png]
 end
