6c3d4d1c598eed9f2f7afbfc8d72fcaa65439259
Alwin Schuster <alwin.schuster@chello.at>
2022-02-15 19:18:01 +0100

    Add graphviz-py support

---
+++ lib/asciidoctor-diagram/graphviz_py/converter.rb
@@ -0,0 +1,32 @@
+require_relative '../diagram_converter'
+require_relative '../util/cli_generator'
+require_relative '../util/platform'
+
+module Asciidoctor
+  module Diagram
+    # @private
+    class GraphvizPyConverter
+      include DiagramConverter
+      include CliGenerator
+
+      def supported_formats
+        [:png, :pdf, :svg]
+      end
+
+      def collect_options(source)
+        {:layout => source.attr('layout')}
+      end
+
+      def convert(source, format, options)
+        generate_stdin(source.find_command('graphviz-py'), format.to_s, source.to_s) do |tool_path, output_path|
+          args = [tool_path, "-o#{Platform.native_path(output_path)}", "-T#{format.to_s}"]
+
+          layout = options[:layout]
+          args << "-K#{layout}" if layout
+
+          args
+        end
+      end
+    end
+  end
+end
