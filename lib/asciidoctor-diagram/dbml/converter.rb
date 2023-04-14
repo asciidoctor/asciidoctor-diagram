d366b4c28625dd7586bd3c0eb6efcc4dedb360c2
Pepijn Van Eeckhoudt <pepijn@vaneeckhoudt.net>
2023-04-14 20:49:10 +0200

    Add DBML support

---
+++ lib/asciidoctor-diagram/dbml/converter.rb
@@ -0,0 +1,22 @@
+require_relative '../diagram_converter'
+require_relative '../util/cli_generator'
+require_relative '../util/platform'
+
+module Asciidoctor
+  module Diagram
+    # @private
+    class DbmlConverter
+      include DiagramConverter
+      include CliGenerator
+
+
+      def supported_formats
+        [:svg]
+      end
+
+      def convert(source, format, options)
+        generate_stdin_stdout(source.find_command('dbml-renderer'), code)
+      end
+    end
+  end
+end
