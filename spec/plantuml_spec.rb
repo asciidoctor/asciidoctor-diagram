c986564f2bac76cf7bcdebd17b1433d0268811f0
Pepijn Van Eeckhoudt <pepijn@vaneeckhoudt.net>
2023-08-11 15:16:56 +0200

    #429 Add PlantUML nested box test case

--- spec/plantuml_spec.rb
+++ spec/plantuml_spec.rb
@@ -797,4 +797,41 @@ Rel_R(api, db, "Reads/Writes")
     expect(b.attributes['width']).to_not be_nil
     expect(b.attributes['height']).to_not be_nil
   end
+
+  it "should support nested boxes" do
+    doc = <<-eos
+= Hello, PlantUML!
+Doc Writer <doc@example.com>
+
+== First Section
+
+[plantuml,format="png"]
+----
+@startuml
+box "b1"
+     participant "b1_p1"
+     box "b1_b1"
+           participant "b1_b1_p1"
+     end box
+end box
+@enduml
+----
+    eos
+
+    d = load_asciidoc doc
+    expect(d).to_not be_nil
+
+    b = d.find { |bl| bl.context == :image }
+    expect(b).to_not be_nil
+
+    expect(b.content_model).to eq :empty
+
+    target = b.attributes['target']
+    expect(target).to_not be_nil
+    expect(target).to match(/\.png$/)
+    expect(File.exist?(target)).to be true
+
+    expect(b.attributes['width']).to_not be_nil
+    expect(b.attributes['height']).to_not be_nil
+  end
 end
